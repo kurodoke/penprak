8b1c2e9895bbb34448fb04d5abd66c3283ff245f
Kurodoke <ariefsatrio098@gmail.com>
2023-04-28 12:25:45 +0700

    bug fix

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -15,19 +15,21 @@
                     Flasher::setFlash("Error", "Database", "danger");
                 }
 
-                if ($_FILES['fileImg']['type'] == "image/jpeg" || $_FILES['fileImg']['type'] == "image/png"){
-                    if(move_uploaded_file($_FILES['fileImg']['tmp_name'], "./public/img/profile/" . $user . ".jpg")){
-                        if($model->editPhoto($user, 1)){
-                            Flasher::setFlash("Update", "berhasil", "success");
+                if($_FILES['fileImg']['error'] != 4){
+                    if ($_FILES['fileImg']['type'] == "image/jpeg" || $_FILES['fileImg']['type'] == "image/png"){
+                        if(move_uploaded_file($_FILES['fileImg']['tmp_name'], "./public/img/profile/" . $user . ".jpg")){
+                            if($model->editPhoto($user, 1)){
+                                Flasher::setFlash("Update", "berhasil", "success");
+                            } else {
+                                Flasher::setFlash("Error", "Database", "danger");
+                            }
                         } else {
-                            Flasher::setFlash("Error", "Database", "danger");
+                            Flasher::setFlash("Error", "Image upload gagal", "danger");
                         }
                     } else {
-                        Flasher::setFlash("Error", "Image upload gagal", "danger");
+                        Flasher::setFlash("Error", "upload jpg atau png saja", "danger");
                     }
-                } else {
-                    Flasher::setFlash("Error", "upload jpg atau png saja", "danger");
-                }
+                }  
                 $this->location("home");
             } else {
                 $this->location("");
